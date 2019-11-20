11af3509b0c4f38f5d7b131b06796b231921e280
Wu Hao 吴号-IS <WuHao11@saicmotor.com>
2019-11-20 23:11:08 +0800

    java easy day1

---
+++ java/14/LongestCommonPrefix.java
@@ -0,0 +1,8 @@
+/**
+ * @author: create by nadao
+ * @version: v1.0
+ * @description: PACKAGE_NAME
+ * @date:2019/11/19
+ */
+public class LongestCommonPrefix {
+}
---
+++ java/35/SearchInsertPosition.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * @author: create by nadao
+ * @version: v1.0
+ * @description: PACKAGE_NAME
+ * @date:2019/11/19
+ */
+public class SearchInsertPosition {
+}
