74a4c13089b774c4915e64178a7bff333e8da5af
ls7198 <ls7198@163.com>
2019-11-28 20:41:17 +0800

    bfs and stack

---
+++ python/155/Min_Stack.py
@@ -0,0 +1,46 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        if len(self.minStack) == 0:
+            self.minStack.append(x)
+        else:
+            target = min(self.minStack[-1], x)
+            self.minStack.append(target)
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
