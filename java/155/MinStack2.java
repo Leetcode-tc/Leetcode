2e5e8663826802d936e46ed59cd7d7428c181f45
张晓佳 <zhangxiaojia02@meituan.com>
2020-01-26 23:03:41 +0800

    feat: add 155-MinStack.java

---
+++ java/155/MinStack2.java
@@ -0,0 +1,31 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private int min = Integer.MAX_VALUE;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
